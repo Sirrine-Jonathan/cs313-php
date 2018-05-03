c5285edd922937e656637ae3fa7f88265e2023b1
jonsirrine <sir15004@byui.edu>
2018-05-03 14:43:16 -0700

    added teach02

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
+
 <?php
 
+  print "<a href=\"helloWorld.html\">helloWorld.html</a>";
   phpinfo();
 
 ?>
--- web/teach02/teach02.js
+++ web/teach02/teach02.js
@@ -1,4 +1,33 @@
-  function showAlert(){
-  	alert("Clicked");
-  }
-  document.getElementById("btn").addEventListener("click", showAlert);
+/*
+Vanilla JavaScript
+
+window.onload = function(){
+	function showAlert(){
+		alert("Clicked");
+	}
+	document.getElementById("btn").addEventListener("click", showAlert);
+
+	function changeColor(color, div){
+		div.style.backgroundColor = color;
+	}
+	document.getElementById("firstColorBtn").addEventListener("click", function(e){
+		changeColor(document.getElementById("firstColorVal").value, document.getElementById('first'));
+	});
+}
+*/
+
+$(function(){
+	$("#btn").click(function(e){
+		alert("Clicked");
+	});
+	$("#firstColorBtn").click(function(e){
+		$("#first").css("background-color",$("#firstColorVal").val());
+	});
+	$("#hideThird").click(function(e){
+		$("#third").toggle('200');
+		if ($("#hideThird").val() == "Hide third div")
+			$("#hideThird").val("Show third div");
+		else
+			$("#hideThird").val("Hide third div");
+	});
+});
