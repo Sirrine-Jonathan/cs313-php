bf5fc58d60f2f8781ee17022ed55b8e92978a622
jonsirrine <sir15004@byui.edu>
2018-05-04 10:38:07 -0700

    ditched carousel

--- web/index.php
+++ web/index.php
@@ -3,20 +3,11 @@
 <head>
 	<title>home</title>
 	
-	<!-- JQuery to use with Bootstrap js -->
-	<script
-	src="https://code.jquery.com/jquery-3.3.1.min.js"
-	integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-	crossorigin="anonymous"></script>
-	
-	<!-- Bootstrap js for courosel, requires JQuery -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	
 	<!-- Bootstrap css -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	
 	<!-- style and script -->
-	<link href="home.css" />
+	<link rel="stylesheet" href="home.css" />
 	<script src="home.js"></script>
 </head>
 <body>
@@ -24,50 +15,21 @@
 		<h1>Jonathan Sirrine</h1>
 		<h3>CS 313 Web Engineering II</h3><h3><a href="links.php">Assignment Links</a></h3>
 	</div>
-	<div id="carousel" class="carousel slide" data-ride="carousel">
-		<!-- Indicators -->
-		<ol class="carousel-indicators">
-			<li data-target="#carousel" data-slide-to="0" class="active"></li>
-			<li data-target="#carousel" data-slide-to="1"></li>
-			<li data-target="#carousel" data-slide-to="2"></li>
-		</ol>
-
-		<!-- Wrapper for slides -->
-		<div class="carousel-inner">
-			<div class="item active">
-			  <img class="cPic" src="pics/jon.jpg" alt="Jonathan in Washington">
-			  <div class="carousel-caption">
-				<h3>Camano Island</h3>
-				<p>This is me on a small island by close to where I'm living</p>
-			  </div>
+	
+	<div class="main-content">
+		<div class="well">
+			<div class="caption-div">
+				<p>Hi my name is Jonathan Sirrine. I live on Camano Island, WA. I grew up in Minnesota where I 
+				taught and practiced paragliding with my father. After getting married I moved out to WA where I began
+				studying Software Engineering.</p>
+				<p>I spend my days studying and building side projects. This page is the landing page for all my coursework
+				for my web engineering class. Thanks for visiting.</p>
 			</div>
-
-			<div class="item">
-			  <img class="cPic" src="pics/colombia.jpg" alt="Paragliding in Columbia">
-			  <div class="carousel-caption">
-				<h3>Ansermanuevo, Columbia</h3>
-				<p>Flying the apsen 3</p>
-			  </div>
-			</div>
-
-			<div class="item">
-			  <img class="cPic" src="pics/colombia2.jpg" alt="Paragliding in Columbia">
-			  <div class="carousel-caption">
-				<h3>Answermanuevo, Columbia</h3>
-				<p>Another picture of Paragliding</p>
-			  </div>
+			<div class="img-div">
+				<img src="pics/jon.jpg" alt="jonathan" /> 
 			</div>
 		</div>
-
-		<!-- Left and right controls -->
-		<a class="left carousel-control" href="#carousel" data-slide="prev">
-			<span class="glyphicon glyphicon-chevron-left"></span>
-			<span class="sr-only">Previous</span>
-		</a>
-		<a class="right carousel-control" href="#carousel" data-slide="next">
-			<span class="glyphicon glyphicon-chevron-right"></span>
-			<span class="sr-only">Next</span>
-		</a>
 	</div>
+
 </body>
 </html>
