7f85657f0cffcd8e86fd70520fddbf9ab460a365
jonsirrine <sir15004@byui.edu>
2018-05-10 13:39:19 -0700

    2nd look

--- web/teach03/teach03.php
+++ web/teach03/teach03.php
@@ -4,22 +4,24 @@
 <body>
 
   <?php 
-	  echo '<form action="send.php" method="post">
-		<p>name:</p> <input type="text" name="name"><br>
+	<form action="send.php" method="post">
+	<p>name:</p> <input type="text" name="name"><br>
   	<p>E-Mail:</p><input type="text" name="email"><br>
-    <p>Major:</p><br>';
+	<p>Major:</p><br>
     
-    echo '
-  		$majors = array("Computer Science", "Web Design and Development", "Computer Information Technology", "Computer Engineering");
-      for ($i = 0; $i < #majors.size(), $i++){
+	<?php
+	$majors = array("Computer Science", "Web Design and Development", "Computer Information Technology", "Computer Engineering");
+      for ($i = 0; $i < $majors.size(), $i++){
       	echo "<input type='radio' name='major' value='" . $majors[$i] . "'/><br />"; 
-      }';
+      };
+	  
+	?>
     
-    echo '
     Comments:<br>
     <textarea name="comments" rows="4" cols="40"></textarea>Comments: <textarea name="comments"></textarea><br />
 		
-    Continents You've Visited:<br>
+		
+    Continents You've Visited:<br />
     North America <input type="checkbox" name="continents[]" value="na"><br>
     South America <input type="checkbox" name="continents[]" value="sa"><br>
     Europe <input type="checkbox" name="continents[]" value="eu"><br>
