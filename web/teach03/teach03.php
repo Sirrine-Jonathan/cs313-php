994541b3ef468fd2f1cc751ac472d3a88e6f74c5
jonsirrine <sir15004@byui.edu>
2018-05-10 13:45:44 -0700

    fixed array length

--- web/teach03/teach03.php
+++ web/teach03/teach03.php
@@ -4,29 +4,30 @@
 <body>
 
 	<form action="send.php" method="post">
-	<p>name:</p> <input type="text" name="name"><br>
-  	<p>E-Mail:</p><input type="text" name="email"><br>
-	<p>Major:</p><br>
+	<p>name:</p> <input type="text" name="name"><br />
+  	<p>E-Mail:</p><input type="text" name="email"><br />
+	<p>Major:</p><br />
     
 	<?php
-	$majors = array("Computer Science", "Web Design and Development", "Computer Information Technology", "Computer Engineering");
-      for ($i = 0; $i < $majors.size(), $i++){
-      	echo "<input type='radio' name='major' value='" . $majors[$i] . "'/><br />"; 
-      };
+		$majors = array("Computer Science", "Web Design and Development", "Computer Information Technology", "Computer Engineering");
+		$majorsLen = count($majors);
+		for ($i = 0; $i < $majorsLen, $i++){
+			echo "<input type='radio' name='major' value='" . $majors[$i] . "'/><br />"; 
+		};
 	  
 	?>
     
-    Comments:<br>
+    Comments:<br />
     <textarea name="comments" rows="4" cols="40"></textarea>Comments: <textarea name="comments"></textarea><br />
 		
 		
     Continents You've Visited:<br />
-    North America <input type="checkbox" name="continents[]" value="na"><br>
-    South America <input type="checkbox" name="continents[]" value="sa"><br>
-    Europe <input type="checkbox" name="continents[]" value="eu"><br>
-    Asia <input type="checkbox" name="continents[]" value="as"><br>
-    Africa <input type="checkbox" name="continents[]" value="af"><br>
-    Antarctica <input type="checkbox" name="continents[]" value="an"><br>
+    North America <input type="checkbox" name="continents[]" value="na"><br />
+    South America <input type="checkbox" name="continents[]" value="sa"><br />
+    Europe <input type="checkbox" name="continents[]" value="eu"><br />
+    Asia <input type="checkbox" name="continents[]" value="as"><br />
+    Africa <input type="checkbox" name="continents[]" value="af"><br />
+    Antarctica <input type="checkbox" name="continents[]" value="an"><br />
     
     </form>
   
